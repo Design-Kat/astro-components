29294d206b3dcc01eaa06f1494267aa92bbd0b6a
Sarah Rudder <sarahr@rocketcom.com>
2019-04-30 18:36:21 -0500

    Add simple notes

--- stories/rux-toggle.stories.js
+++ stories/rux-toggle.stories.js
@@ -6,10 +6,17 @@ import { RuxToggle } from '../src/components/rux-toggle/rux-toggle.js';
 
 storiesOf('Components|Toggle', module)
   .addDecorator(withKnobs)
-  .add('Toggle', () => {
-    const disabled = boolean('Disabled', false);
-    const checked = boolean('Checked', false);
-    return html`
-      <rux-toggle .disabled=${disabled} .checked=${checked}></rux-toggle>
-    `;
-  });
+  .add(
+    'Toggle',
+    () => {
+      const disabled = boolean('Disabled', false);
+      const checked = boolean('Checked', false);
+      return html`
+        <rux-toggle .disabled=${disabled} .checked=${checked}></rux-toggle>
+      `;
+    },
+    {
+      notes:
+        'A Toggle describes a state or value. Similar to a checkbox toggles allow users to change a setting between two states such as “On" or "Off.” Unlike a checkbox, a toggle button initiates an action with immediate effect.',
+    },
+  );
