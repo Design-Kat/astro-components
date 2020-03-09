70b4d8036767bd10362e643053085784dbce34aa
Duncan McAlester <duncan@rocketcom.com>
2020-03-09 15:31:18 -0700

    Added test for attribute/property

--- src/components/rux-status/rux-status.test.js
+++ src/components/rux-status/rux-status.test.js
@@ -13,4 +13,46 @@ describe('Astro Status Indicator', () => {
 		);
 		expect(el).shadowDom.to.equal(`<div></div>`);
 		});
+	it('is accessible', async() => {
+		const el = await fixture(
+      html `<rux-status status="off"></rux-status>`
+		);
+
+		expect(el).shadowDom.to.be.accessible;
+	});
+	it('can instantiate an element with properties', async () => {
+		const el = await fixture(
+			html`
+				<rux-status .status=${'critical'}></rux-status>
+			`,
+		);
+		expect(el.status).to.equal('critical');
+	});
+	it('status property updates with attribute change', async () => {
+		const el = await fixture(
+      html `<rux-status .status=${'off'}></rux-status>`
+		);
+		expect(el.status).to.eq('off');
+
+		el.status = 'standby';
+		await elementUpdated(el);
+		expect(el.status).to.equal('standby');
+
+		el.status = 'normal';
+		await elementUpdated(el);
+		expect(el.status).to.equal('normal');
+
+		el.status = 'caution';
+		await elementUpdated(el);
+		expect(el.status).to.equal('caution');
+
+		el.status = 'serious';
+		await elementUpdated(el);
+		expect(el.status).to.equal('serious');
+
+		el.status = 'critical';
+		await elementUpdated(el);
+		expect(el.status).to.equal('critical');
+
+	});
 });
